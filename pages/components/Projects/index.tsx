b003829c9717e06609f0c0d7b55959e18f1c7b52
oseeshogun <omasuaku@gmail.com>
2022-07-25 14:26:22 +0100

    Scroll horizontal event

--- pages/components/Projects/index.tsx
+++ pages/components/Projects/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from "react";
 import styles from "../../../styles/Projects.module.css";
 import ProjectItem from "./ProjectItem";
 import { IoChevronForwardOutline, IoChevronBackSharp } from "react-icons/io5";
@@ -11,201 +11,90 @@ function Projects({
   setEndTouched: Dispatch<SetStateAction<boolean>>;
 }) {
   const projectsListRef = useRef<HTMLUListElement | null>(null);
+  const refContainer = useRef<HTMLDivElement>(null);
 
-  const [listIsIntercepting, setListIsIntercepting] = useState<boolean>(false);
-  const [keepScroll, setKeepScroll] = useState<boolean>(true);
-  const [y, setY] = useState<number>(0);
-  const [fixYsetted, setFixYsetted] = useState<boolean>(false);
-  const [fixY, setFixY] = useState<number>(0);
-  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 720) {
-        setIsMobile(true);
-        setEndTouched(true);
-      } else {
-        setIsMobile(false);
-      }
-    };
+  const [scrollY, setScrollY] = useState<number>(0);
 
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [setEndTouched]);
+  let progress = 0;
+
+  const handleScroll = useCallback(() => {
+    setScrollY(window.scrollY);
+  }, []);
+
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const entry = entries[0];
-      const beginHorizontalScroll =
-        entry.boundingClientRect.y < -40 && !keepScroll;
-      setListIsIntercepting(beginHorizontalScroll);
+    document.addEventListener("scroll", handleScroll, { passive: true });
 
-      if (beginHorizontalScroll && !fixYsetted) {
-        setFixY(window.scrollY);
-        setFixYsetted(true);
-      }
-      if (entry.isIntersecting && keepScroll) {
-        setKeepScroll(false);
-      }
-      if (!entry.isIntersecting) {
-        // ReInit
-        // setListIsIntercepting(false);
-        // setKeepScroll(true);
-        // setEndTouched(false);
-        // projectsListRef.current?.scrollTo(30, 0);
-      }
-    });
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
 
-    observer.observe(projectsListRef.current!);
+  const { current: elContainer } = refContainer;
 
-    const onScroll = (event: Event) => {
-      if (event.target) {
-        // console.log(
-        //   "Reached bottom ",
-        //   projectsListRef.current.scrollWidth,
-        //   projectsListRef.current.scrollLeft,
-        //   projectsListRef.current.clientWidth
-        // );
-        const { scrollLeft, scrollWidth, clientWidth } =
-          projectsListRef.current as HTMLUListElement;
+  if (elContainer) {
+    const { clientHeight, offsetTop } = elContainer;
+    const screenH = window.innerHeight;
+    const halfH = screenH / 2;
+    const percentY =
+      Math.min(
+        clientHeight + halfH,
+        Math.max(-screenH, scrollY - offsetTop) + halfH
+      ) / clientHeight;
+    progress = percentY < 0 ? 0 : (percentY > 1 ? 1 : percentY);
+  }
 
-        if (scrollLeft == scrollWidth - clientWidth) {
-          // TO SOMETHING HERE
+  const getTransform: (progress: number) => string = (progress: number) => {
+    const scrollSize = window.innerWidth > 700 ? 180 : 240;
+    return `translateX(-${(progress * scrollSize)}vw)`;
+  }
 
-          setListIsIntercepting(false);
-          setKeepScroll(true);
-          setEndTouched(true);
-        }
-      }
-    };
-
-    const listProjects = projectsListRef.current;
-
-    listProjects?.addEventListener("scroll", onScroll);
-
-    const handleNavigation = (event: Event) => {
-      const window = event.currentTarget as Window;
-
-      if (listIsIntercepting && !endTouched) {
-        // document.body.style.overflow = "hidden";
-        // window.scrollTo(y)
-        // window.scrollTo(0, fixY);
-      }
-
-      if (y > window.scrollY) {
-        // console.log("scrolling up");
-        // console.log("scrolling", event.pageYOffset);
-        if (listIsIntercepting) {
-          projectsListRef.current?.scrollBy(-10, 0);
-        }
-      } else {
-        // console.log("scrolling down");
-        if (listIsIntercepting) {
-          projectsListRef.current?.scrollBy(10, 0);
-        }
-      }
-      setY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleNavigation);
-    return () => {
-      // return a cleanup function to unregister our function since its gonna run multiple times
-      window.removeEventListener("scroll", handleNavigation);
-      listProjects?.removeEventListener("scroll", onScroll);
-    };
-  }, [
-    y,
-    listIsIntercepting,
-    keepScroll,
-    endTouched,
-    fixY,
-    fixYsetted,
-    setEndTouched,
-  ]);
-
-  const goBackward = () => {
-    const { scrollLeft, scrollWidth, clientWidth } =
-      projectsListRef.current as HTMLUListElement;
-    const width = scrollWidth - clientWidth;
-    const singleWidth = width / 3;
-    projectsListRef.current?.scrollBy({
-      top: 0,
-      left: -singleWidth,
-      behavior: "smooth",
-    });
-  };
-
-  const goForward = () => {
-    const { scrollLeft, scrollWidth, clientWidth } =
-      projectsListRef.current as HTMLUListElement;
-    const width = scrollWidth - clientWidth;
-    const singleWidth = width / 3;
-    projectsListRef.current?.scrollBy({
-      top: 0,
-      left: singleWidth,
-      behavior: "smooth",
-    });
-  };
+  const getLeft: (index: number) => string = (index: number) => {
+    return 30 + (index * 80) + 'vw';
+  }
 
   return (
-    <div className={styles.container} id="projects">
+    <div className={styles.container} ref={refContainer} id="projects">
       <div className={styles.titleContainer}>
         <h1 className={styles.title}>Some things I’ve worked on</h1>
         <div className={styles.underline}></div>
       </div>
 
-      <div className={styles.projects}>
-        <IoChevronBackSharp
-          className={styles.prevIcon}
-          color="#8f94fb"
-          size={50}
-          onClick={(e) => goBackward()}
-        />
-        <IoChevronForwardOutline
-          className={styles.nextIcon}
-          color="#8f94fb"
-          size={50}
-          onClick={(e) => goForward()}
-        />
-        <ul className={styles.projectsList} ref={projectsListRef}>
-          <li key={1}>
-            <ProjectItem
-              title="Nab LifeStyle"
-              description="A commercial raffle app where people buy tickets and win prices."
-              technoDescription="Flutter - Express - Sequelize - Heroku - Mysql"
-              owner="Faithful SARL"
-              ownerLink="https://www.faithful-tech.com/"
-              imageUrl="/images/Frame 1.png"
-              index={1}
-            />
-          </li>
-          <li key={2}>
-            <ProjectItem
-              title="Masolo"
-              description="A chat app where people can send messages and make phone calls."
-              technoDescription=""
-              owner="Etidlabs"
-              ownerLink="http://etidlabs.com/"
-              imageUrl="/images/Frame 2.png"
-              index={2}
-            />
-          </li>
-          <li key={3}>
-            <ProjectItem
-              title="Bongola Chat"
-              description="A chat app with a chat bot, games, news notifications and voice calls."
-              technoDescription="Flutter - Firebase -  JavaScript -Express- 
+      <ul className={styles.projectsList} ref={projectsListRef}      >
+        <li key={1} style={{ left: getLeft(0), transform: getTransform(progress) }}>
+          <ProjectItem
+            title="Nab LifeStyle"
+            description="A commercial raffle app where people buy tickets and win prices."
+            technoDescription="Flutter - Express - Sequelize - Heroku - Mysql"
+            owner="Faithful SARL"
+            ownerLink="https://www.faithful-tech.com/"
+            imageUrl="/images/Frame 1.png"
+            index={1}
+          />
+        </li>
+        <li key={2} style={{ left: getLeft(1), transform: getTransform(progress) }}>
+          <ProjectItem
+            title="Masolo"
+            description="A chat app where people can send messages and make phone calls."
+            technoDescription=""
+            owner="Etidlabs"
+            ownerLink="http://etidlabs.com/"
+            imageUrl="/images/Frame 2.png"
+            index={2}
+          />
+        </li>
+        <li key={3} style={{ left: getLeft(2), transform: getTransform(progress) }}>
+          <ProjectItem
+            title="Bongola Chat"
+            description="A chat app with a chat bot, games, news notifications and voice calls."
+            technoDescription="Flutter - Firebase -  JavaScript -Express- 
               Python - Django - MongoDB"
-              owner={undefined}
-              ownerLink={undefined}
-              imageUrl="/images/Frame 3.png"
-              index={3}
-            />
-          </li>
-        </ul>
-      </div>
+            owner={undefined}
+            ownerLink={undefined}
+            imageUrl="/images/Frame 3.png"
+            index={3}
+          />
+        </li>
+      </ul>
     </div>
   );
 }
